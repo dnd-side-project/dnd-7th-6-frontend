010194ad5866d78b3207632ed88168bbe165bc6e
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-07-30 00:46:47 +0900

    ✨ feat: add fonts & edit Design Theme (#43)
    Theme.ts에 branding 완료된 Typograpghy 폰트 추가하고 android, ios 각각 따로 설정 해주었습니다.
    
    #43

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -1,11 +1,18 @@
 import {Theme} from '@emotion/react';
 
+import {fontPercentage} from './ScreenResponse';
+
 declare module '@emotion/react' {
   export interface Theme {
     colors: {
       primary: PrimaryColorTheme;
       secondary: Array<SecondaryColorTheme>;
       grayscale: Array<string>;
+    };
+    fonts: {
+      Headline: Array<HeadLineFontsTheme>;
+      SubHeadline: Array<HeadLineFontsTheme>;
+      Body: Array<HeadLineFontsTheme>;
     };
   }
 }
@@ -18,6 +25,12 @@ export interface SecondaryColorTheme {
   light: string;
   normal: string;
   dark: string;
+}
+
+export interface HeadLineFontsTheme {
+  size: number;
+  fontFamily: string;
+  lineHeight: number;
 }
 
 const theme: Theme = {
@@ -42,6 +55,64 @@ const theme: Theme = {
       '#4C4C4C',
       '#333333',
       '#191919',
+    ],
+  },
+  fonts: {
+    Headline: [
+      {
+        size: fontPercentage(28),
+        fontFamily: 'Pretendard-SemiBold',
+        lineHeight: fontPercentage(28) * 1.28,
+      },
+      {
+        size: fontPercentage(24),
+        fontFamily: 'Pretendard-SemiBold',
+        lineHeight: fontPercentage(24) * 1.3,
+      },
+      {
+        size: fontPercentage(22),
+        fontFamily: 'Pretendard-SemiBold',
+        lineHeight: fontPercentage(22) * 1.3,
+      },
+    ],
+    SubHeadline: [
+      {
+        size: fontPercentage(20),
+        fontFamily: 'Pretendard-Medium',
+        lineHeight: fontPercentage(20) * 1.3,
+      },
+      {
+        size: fontPercentage(18),
+        fontFamily: 'Pretendard-Medium',
+        lineHeight: fontPercentage(18) * 1.36,
+      },
+      {
+        size: fontPercentage(14),
+        fontFamily: 'Pretendard-Medium',
+        lineHeight: fontPercentage(14) * 1.36,
+      },
+    ],
+    Body: [
+      {
+        size: fontPercentage(16),
+        fontFamily: 'Pretendard-Medium',
+        lineHeight: fontPercentage(16) * 1.36,
+      },
+      {
+        size: fontPercentage(16),
+        fontFamily: 'Pretendard-Regular',
+        lineHeight: fontPercentage(16) * 1.36,
+      },
+      {
+        size: fontPercentage(14),
+        fontFamily: 'Pretendard-Regular',
+        lineHeight: fontPercentage(14) * 1.4,
+      },
+      {
+        size: fontPercentage(12),
+        fontFamily: 'Pretendard-Regular',
+        lineHeight: fontPercentage(12) * 1.4,
+      },
     ],
   },
 };
