44e10e517f580c3b41b091c4e25bf9e9723d8bf9
vgihan <jum654@naver.com>
2022-07-30 21:11:44 +0900

    ✨ feat(detaildiff): add DetailDiffOrganism Component (#18)
    API 호출 없는 임시 컴포넌트로 구현
    
    #18

--- src/components/RecommendDetailDiffOrganism/RecommendDetailDiffOrganism.tsx
+++ src/components/RecommendDetailDiffOrganism/RecommendDetailDiffOrganism.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
 
-import {CardWrapper, Container} from './RecommendDetailDiffOrganism.styles';
+import RecommendFeedCard from '../RecommendFeedCard';
+import {PressableRightArrowIcon} from '../utils/Pressables/PressableIcons';
+import {SubHeadline2} from '../utils/Text';
+import {
+  CardContainer,
+  CardWrapper,
+  Container,
+  IconWrapper,
+  NumOfDiff,
+  Title,
+  TitleContainer,
+} from './RecommendDetailDiffOrganism.styles';
 
 const RecommendDetailDiffOrganism = () => {
   return (
     <Container>
-      <CardWrapper />
+      <TitleContainer>
+        <SubHeadline2>
+          <Title>minbberry님의 다른 사진 </Title>
+          <NumOfDiff> 33</NumOfDiff>
+        </SubHeadline2>
+        <IconWrapper>
+          <PressableRightArrowIcon />
+        </IconWrapper>
+      </TitleContainer>
+      <CardContainer>
+        <CardWrapper>
+          <RecommendFeedCard imgUrl="" />
+        </CardWrapper>
+        <CardWrapper>
+          <RecommendFeedCard imgUrl="" />
+        </CardWrapper>
+        <CardWrapper>
+          <RecommendFeedCard imgUrl="" />
+        </CardWrapper>
+        <CardWrapper>
+          <RecommendFeedCard imgUrl="" />
+        </CardWrapper>
+      </CardContainer>
     </Container>
   );
 };
