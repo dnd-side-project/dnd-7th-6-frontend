c7089b002eb1b0101cf3e956ff42ae15978d332d
vgihan <jum654@naver.com>
2022-07-30 21:10:05 +0900

    ✨ feat(pressables): add PressableRightArrowIcon Component (#18)
    #18

--- src/components/utils/Pressables/PressableIcons.tsx
+++ src/components/utils/Pressables/PressableIcons.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PressableIcon from '../PressableIcon';
 
 import LikeIcon from 'src/icons/LikeIcon';
+import RightArrowIcon from 'src/icons/RightArrowIcon';
 import UploadIcon from 'src/icons/UploadIcon';
 
 export const PressableUploadIcon = () => {
@@ -17,6 +18,14 @@ export const PressableLikeIcon = () => {
   return (
     <PressableIcon>
       <LikeIcon />
+    </PressableIcon>
+  );
+};
+
+export const PressableRightArrowIcon = () => {
+  return (
+    <PressableIcon>
+      <RightArrowIcon />
     </PressableIcon>
   );
 };
