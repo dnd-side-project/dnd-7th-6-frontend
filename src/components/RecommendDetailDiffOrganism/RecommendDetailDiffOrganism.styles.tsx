dca104248b5f91c6ea25ead34cc15872bab9a71e
vgihan <jum654@naver.com>
2022-07-31 21:16:11 +0900

    ✨ feat(detaildiff): modify style & add data fetch logic
    Container에서 padding으로 처리하던 작업을, TitleContainer의 width를 고정으로 지정함으로써 수정

--- src/components/RecommendDetailDiffOrganism/RecommendDetailDiffOrganism.styles.tsx
+++ src/components/RecommendDetailDiffOrganism/RecommendDetailDiffOrganism.styles.tsx
@@ -4,13 +4,14 @@ import {heightPercentage, widthPercentage} from 'src/styles/ScreenResponse';
 import theme from 'src/styles/Theme';
 
 export const Container = styled.View({
-  paddingHorizontal: widthPercentage(16),
   backgroundColor: 'white',
 });
 
 export const TitleContainer = styled.View({
   flexDirection: 'row',
+  width: widthPercentage(343),
   height: heightPercentage(56),
+  marginHorizontal: widthPercentage(16),
   backgroundColor: theme.colors.grayscale[2],
   alignItems: 'center',
   justifyContent: 'space-between',
