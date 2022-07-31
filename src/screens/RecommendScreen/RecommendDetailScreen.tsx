2ee13ecb11596ee915c1baed1c213b81e574079d
vgihan <jum654@naver.com>
2022-07-31 19:30:23 +0900

    ♻️ refactor(detailscreen): divide Logic (Screen & Header style) (#21)
    #21

--- src/screens/RecommendScreen/RecommendDetailScreen.tsx
+++ src/screens/RecommendScreen/RecommendDetailScreen.tsx
@@ -1,5 +1,5 @@
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import React, {useEffect} from 'react';
+import React, {useLayoutEffect} from 'react';
 import {SafeAreaView, ScrollView} from 'react-native';
 
 import {RecommendParamList} from '.';
@@ -7,21 +7,13 @@ import {RecommendParamList} from '.';
 import RecommendDetailContentsOrganism from 'src/components/RecommendDetailContentsOrganism';
 import RecommendDetailDiffOrganism from 'src/components/RecommendDetailDiffOrganism';
 import RecommendDetailMainFrame from 'src/components/RecommendDetailMainFrame';
-import {PressableLeftArrowIcon} from 'src/components/utils/Pressables/PressableIcons';
+import {DetailHeaderStyle} from './RecommendDetailScreen.header';
 
-type DetailScreenProps = NativeStackScreenProps<RecommendParamList, 'RecommendDetail'>;
+export type DetailScreenProps = NativeStackScreenProps<RecommendParamList, 'RecommendDetail'>;
 
-const RecommendDetailScreen = ({route, navigation}: DetailScreenProps) => {
-  const headerStyle = () => {
-    navigation.setOptions({
-      headerTransparent: true,
-      headerLeft: () => <PressableLeftArrowIcon onPress={() => navigation.goBack()} />,
-      title: '',
-    });
-  };
-
-  useEffect(() => {
-    headerStyle();
+const RecommendDetailScreen = ({navigation, route}: DetailScreenProps) => {
+  useLayoutEffect(() => {
+    DetailHeaderStyle({navigation, route});
   });
 
   return (
