a16a95bbf4f03bce9ae81a396cf67cbdccf2896b
vgihan <jum654@naver.com>
2022-08-01 19:17:22 +0900

    ✨ feat(bottomsheet): wrapping MapBottomSheetOrganism using gestureHandlerRootHOC (#60)
    #60

--- src/components/MapBottomSheetOrganism/MapBottomSheetOrganism.tsx
+++ src/components/MapBottomSheetOrganism/MapBottomSheetOrganism.tsx
@@ -1,27 +1,23 @@
 import BottomSheet from '@gorhom/bottom-sheet';
-import React, {useMemo, useRef, useState} from 'react';
-import {Button} from 'react-native';
+import React, {useMemo, useRef} from 'react';
+import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
 
 import {bottomSheetStyle, handleStyle} from './MapBottomSheetOrganism.styles';
 
 const MapBottomSheetOrganism = () => {
   const bottomSheetRef = useRef<BottomSheet>(null);
   const snapPoints = useMemo(() => ['25%', '70%'], []);
-  const [sheetIndex, setSheetIndex] = useState(-1);
 
   return (
     <>
-      <Button title="up" onPress={() => setSheetIndex(1)} />
-      <Button title="center" onPress={() => setSheetIndex(0)} />
       <BottomSheet
         style={bottomSheetStyle}
         snapPoints={snapPoints}
         ref={bottomSheetRef}
-        index={sheetIndex}
         handleIndicatorStyle={handleStyle}
       />
     </>
   );
 };
 
-export default MapBottomSheetOrganism;
+export default gestureHandlerRootHOC(MapBottomSheetOrganism);
