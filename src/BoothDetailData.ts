e65a066e1ae9a53aa767ff3dcd5c55a9376e1fb4
vgihan <jum654@naver.com>
2022-08-02 16:17:25 +0900

    🧪 test(boothdetaildata): add BoothDetailData (#74)
    부스 상세 mock data 추가
    
    #74

---
+++ src/BoothDetailData.ts
@@ -0,0 +1,50 @@
+const BoothDetailData = {
+  title: '인생네컷 익선동지점',
+  address: '서울 종로구 돈화문로 11가길 29 102호 (우) 03133',
+  activeTime: ['00:00', '24:00'],
+  keyword: {
+    total: 9999,
+    elements: [
+      {title: '깨끗해요', numOfKeyword: 99999},
+      {title: '소품이 다양해요', numOfKeyword: 126},
+      {title: '파우더룸이 잘 되어있어요', numOfKeyword: 50},
+      {title: '인테리어가 멋져요', numOfKeyword: 50},
+    ],
+  },
+  review: {
+    total: 9999,
+    elements: [
+      {
+        writer: 'minbberry',
+        date: '2023. 12. 30',
+        contents:
+          '사진 잘나오고 모자 종류 많음 물론 정리 상태는 엉망이에요! 그래도 찾으면 건질게 꽤 많음 저녁 10시쯤 갔는데 에어컨 빵빵해서 3번이나 찍었습니다~',
+      },
+      {
+        writer: 'minbberry',
+        date: '2023. 12. 30',
+        contents:
+          '사진 잘나오고 모자 종류 많음 물론 정리 상태는 엉망이에요! 그래도 찾으면 건질게 꽤 많음 저녁 10시쯤 갔는데 에어컨 빵빵해서 3번이나 찍었습니다~',
+      },
+      {
+        writer: 'minbberry',
+        date: '2023. 12. 30',
+        contents:
+          '사진 잘나오고 모자 종류 많음 물론 정리 상태는 엉망이에요! 그래도 찾으면 건질게 꽤 많음 저녁 10시쯤 갔는데 에어컨 빵빵해서 3번이나 찍었습니다~',
+      },
+    ],
+  },
+  boothPhoto: {
+    total: 9999,
+    elements: [
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_NHQH8W9koZ55iRPtxTaRnbNdQPk8sW1qfQ&usqp=CAU',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_NHQH8W9koZ55iRPtxTaRnbNdQPk8sW1qfQ&usqp=CAU',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_NHQH8W9koZ55iRPtxTaRnbNdQPk8sW1qfQ&usqp=CAU',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_NHQH8W9koZ55iRPtxTaRnbNdQPk8sW1qfQ&usqp=CAU',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_NHQH8W9koZ55iRPtxTaRnbNdQPk8sW1qfQ&usqp=CAU',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_NHQH8W9koZ55iRPtxTaRnbNdQPk8sW1qfQ&usqp=CAU',
+    ],
+  },
+};
+
+export default BoothDetailData;
