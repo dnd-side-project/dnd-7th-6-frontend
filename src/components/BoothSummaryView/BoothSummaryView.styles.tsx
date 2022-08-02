0b741cc7a2274a88f68e8a8ad166d986fa94d87f
vgihan <jum654@naver.com>
2022-08-02 14:10:19 +0900

    ✨ feat(boothsummary): modify style in BoothSummaryView (cls)
    cls

--- src/components/BoothSummaryView/BoothSummaryView.styles.tsx
+++ src/components/BoothSummaryView/BoothSummaryView.styles.tsx
@@ -2,25 +2,38 @@ import styled from '@emotion/native';
 
 import {BodyText5, BodyText6, SubHeadline2} from '../utils/Text';
 
-import {heightPercentage} from 'src/styles/ScreenResponse';
+import {heightPercentage, widthPercentage} from 'src/styles/ScreenResponse';
 import theme from 'src/styles/Theme';
 
 export const Container = styled.Pressable({
-  paddingVertical: heightPercentage(16),
   flexDirection: 'row',
   justifyContent: 'space-between',
+  paddingBottom: heightPercentage(18),
+  paddingTop: heightPercentage(16),
+  paddingHorizontal: widthPercentage(16),
+  borderBottomWidth: 1,
+  borderBottomColor: theme.colors.grayscale[2],
 });
 
 export const DescriptionContainer = styled.View({});
 
 export const TagContainer = styled.View({
   flexDirection: 'row',
+  paddingVertical: heightPercentage(4),
 });
 
 export const ImageContainer = styled.View({});
 
+export const ChipWrapper = styled.View({
+  paddingRight: widthPercentage(4),
+});
+
 export const Title = styled(SubHeadline2)({
   color: theme.colors.grayscale[9],
+});
+
+export const SubText = styled.Text({
+  marginVertical: heightPercentage(4),
 });
 
 export const Distance = styled(BodyText5)({
--- src/components/BoothSummaryView/BoothSummaryView.tsx
+++ src/components/BoothSummaryView/BoothSummaryView.tsx
@@ -3,29 +3,35 @@ import {Text} from 'react-native';
 
 import RoundChip from '../Chip/RoundChip';
 import {
+  ChipWrapper,
   Container,
   DescriptionContainer,
   Distance,
   ImageContainer,
   Review,
+  SubText,
   TagContainer,
   Title,
 } from './BoothSummaryView.styles';
 
 import {BoothSummary} from 'src/types';
+import toLocaleString from 'src/utils/toLocaleString';
 
 const BoothSummaryView = (props: BoothSummary) => {
   return (
     <Container>
       <DescriptionContainer>
         <Title>{props.title}</Title>
-        <Text>
-          <Distance>{props.distance}</Distance>
-          <Review>{props.review}</Review>
-        </Text>
+        <SubText>
+          <Distance>{props.distance}km</Distance>
+          <Text> | </Text>
+          <Review>부스리뷰 {toLocaleString(props.review)}개</Review>
+        </SubText>
         <TagContainer>
           {props.tags.map(tag => (
-            <RoundChip>{tag}</RoundChip>
+            <ChipWrapper key={tag}>
+              <RoundChip mode="gray">{tag}</RoundChip>
+            </ChipWrapper>
           ))}
         </TagContainer>
       </DescriptionContainer>
