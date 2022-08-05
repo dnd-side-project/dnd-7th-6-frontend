41a591e3a1739bfddd6bea4436a4e266ab9d7f21
vgihan <jum654@naver.com>
2022-08-06 00:10:15 +0900

    ✨ feat(redux): add focusedFilter global state in redux store

--- src/redux/actions/PostAction.ts
+++ src/redux/actions/PostAction.ts
@@ -1,4 +1,4 @@
-import {CLOSE_FILTER_SHEET, OPEN_FILTER_SHEET} from '../types/PostActionType';
+import {CHANGE_FOCUS, CLOSE_FILTER_SHEET, OPEN_FILTER_SHEET} from '../types/PostActionType';
 
 export const openFilterSheet = () => ({
   type: OPEN_FILTER_SHEET,
@@ -6,4 +6,9 @@ export const openFilterSheet = () => ({
 
 export const closeFilterSheet = () => ({
   type: CLOSE_FILTER_SHEET,
+});
+
+export const changeFocus = (focus: number) => ({
+  type: CHANGE_FOCUS,
+  payload: {focus},
 });
--- src/redux/reducers/postReducer.ts
+++ src/redux/reducers/postReducer.ts
@@ -1,17 +1,22 @@
 import {Reducer} from 'redux';
 
-import {CLOSE_FILTER_SHEET, OPEN_FILTER_SHEET} from '../types/PostActionType';
+import {CHANGE_FOCUS, CLOSE_FILTER_SHEET, OPEN_FILTER_SHEET} from '../types/PostActionType';
 
 const initialState = {
   isOpenFilterSheet: false,
+  focusedFilter: 0,
 };
 
 const postReducer: Reducer = (state = initialState, action) => {
+  const {payload} = action;
+
   switch (action.type) {
     case OPEN_FILTER_SHEET:
       return {...state, isOpenFilterSheet: true};
     case CLOSE_FILTER_SHEET:
       return {...state, isOpenFilterSheet: false};
+    case CHANGE_FOCUS:
+      return {...state, focusedFilter: payload.focus};
     default:
       return state;
   }
