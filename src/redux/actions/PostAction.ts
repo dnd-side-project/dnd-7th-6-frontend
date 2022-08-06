d1d603800fa7bae8aeafb16cb0bde2fb641d21c5
vgihan <jum654@naver.com>
2022-08-06 20:43:26 +0900

    ✨ feat(redux): add Action of Change Filter Tag
    브랜드, 인원, 포즈컨셉, 프레임 필터 태그에 대한 global state 정의 및 액션 생성

--- src/redux/actions/PostAction.ts
+++ src/redux/actions/PostAction.ts
@@ -1,4 +1,9 @@
-import {CHANGE_FOCUS, CLOSE_FILTER_SHEET, OPEN_FILTER_SHEET} from '../types/PostActionType';
+import {
+  CHANGE_FILTER,
+  CHANGE_FOCUS,
+  CLOSE_FILTER_SHEET,
+  OPEN_FILTER_SHEET,
+} from '../types/PostActionType';
 
 export const openFilterSheet = () => ({
   type: OPEN_FILTER_SHEET,
@@ -11,4 +16,34 @@ export const closeFilterSheet = () => ({
 export const changeFocus = (focus: number) => ({
   type: CHANGE_FOCUS,
   payload: {focus},
+});
+
+export const changeFilteredBrand = (target: number) => ({
+  type: CHANGE_FILTER.BRAND,
+  payload: {target},
+});
+
+export const changeFilteredHeadcountNumber = (target: number) => ({
+  type: CHANGE_FILTER.HEADCOUNT.NUMBER,
+  payload: {target},
+});
+
+export const changeFilteredHeadcountRelation = (target: number) => ({
+  type: CHANGE_FILTER.HEADCOUNT.RELATION,
+  payload: {target},
+});
+
+export const changeFilteredPoseEmotion = (target: number) => ({
+  type: CHANGE_FILTER.POSE.EMOTION,
+  payload: {target},
+});
+
+export const changeFilteredPoseSituation = (target: number) => ({
+  type: CHANGE_FILTER.POSE.SITUATION,
+  payload: {target},
+});
+
+export const changeFilteredFrame = (target: number) => ({
+  type: CHANGE_FILTER.FRAME,
+  payload: {target},
 });
--- src/redux/reducers/postReducer.ts
+++ src/redux/reducers/postReducer.ts
@@ -1,10 +1,27 @@
 import {Reducer} from 'redux';
 
-import {CHANGE_FOCUS, CLOSE_FILTER_SHEET, OPEN_FILTER_SHEET} from '../types/PostActionType';
+import {
+  CHANGE_FILTER,
+  CHANGE_FOCUS,
+  CLOSE_FILTER_SHEET,
+  OPEN_FILTER_SHEET,
+} from '../types/PostActionType';
+
+import toggleTag from 'src/utils/toggleTag';
 
 const initialState = {
   isOpenFilterSheet: false,
   focusedFilter: 0,
+  filteredBrand: {},
+  filteredHeadcount: {
+    number: {},
+    relation: {},
+  },
+  filteredPose: {
+    emotion: {},
+    situation: {},
+  },
+  filteredFrame: {},
 };
 
 const postReducer: Reducer = (state = initialState, action) => {
@@ -17,6 +34,30 @@ const postReducer: Reducer = (state = initialState, action) => {
       return {...state, isOpenFilterSheet: false};
     case CHANGE_FOCUS:
       return {...state, focusedFilter: payload.focus};
+    case CHANGE_FILTER.BRAND:
+      return {...state, filteredBrand: toggleTag(state.filteredBrand, payload.target)};
+    case CHANGE_FILTER.FRAME:
+      return {...state, filteredFrame: toggleTag(state.filteredFrame, payload.target)};
+    case CHANGE_FILTER.HEADCOUNT.NUMBER:
+      return {
+        ...state,
+        filteredHeadcount: {number: toggleTag(state.filteredHeadcount.number, payload.target)},
+      };
+    case CHANGE_FILTER.HEADCOUNT.RELATION:
+      return {
+        ...state,
+        filteredHeadcount: {relation: toggleTag(state.filteredHeadcount.relation, payload.target)},
+      };
+    case CHANGE_FILTER.POSE.EMOTION:
+      return {
+        ...state,
+        filteredPose: {emotion: toggleTag(state.filteredPose.emotion, payload.target)},
+      };
+    case CHANGE_FILTER.POSE.SITUATION:
+      return {
+        ...state,
+        filteredPose: {situation: toggleTag(state.filteredPose.situation, payload.target)},
+      };
     default:
       return state;
   }
