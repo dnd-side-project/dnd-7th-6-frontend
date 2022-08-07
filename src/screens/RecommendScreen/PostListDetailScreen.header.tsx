2ffef44630ec554a561e4e8af371594fb42fcb20
vgihan <jum654@naver.com>
2022-08-07 18:21:08 +0900

    ✨ feat: add search button in PostListDetailScreen

--- src/screens/RecommendScreen/PostListDetailScreen.header.tsx
+++ src/screens/RecommendScreen/PostListDetailScreen.header.tsx
@@ -1,9 +1,13 @@
 import styled from '@emotion/native';
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 
 import {PostListDetailScreenProps} from './PostListDetailScreen';
 
-import {PressableLeftArrowIcon} from 'src/components/utils/Pressables/PressableIcons';
+import {
+  PressableLeftArrowIcon,
+  PressableSearchIcon,
+} from 'src/components/utils/Pressables/PressableIcons';
 import {SubHeadline2} from 'src/components/utils/Text';
 import {heightPercentage, widthPercentage} from 'src/styles/ScreenResponse';
 import theme from 'src/styles/Theme';
@@ -24,15 +28,23 @@ const Container = styled.View({
 const ScreenTitle = styled(SubHeadline2)({
   color: theme.colors.grayscale[9],
 });
+
 const BackButtonWrapper = styled.View({position: 'absolute', left: widthPercentage(16)});
 
+const SearchButtonWrapper = styled.View({position: 'absolute', right: widthPercentage(16)});
+
 export const Header = () => {
+  const navigation = useNavigation();
+
   return (
     <Container>
       <BackButtonWrapper>
-        <PressableLeftArrowIcon />
+        <PressableLeftArrowIcon onPress={() => navigation.goBack()} />
       </BackButtonWrapper>
       <ScreenTitle>포스트</ScreenTitle>
+      <SearchButtonWrapper>
+        <PressableSearchIcon />
+      </SearchButtonWrapper>
     </Container>
   );
 };
