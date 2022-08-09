5704fcedeabdf7faec3533275edab297ec1c2dcb
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-08-10 00:13:07 +0900

    ✨ feat: link focused marker and bottom sheet (#109)
    포커스 마커와 바텀시트 스크롤뷰 내 ui 연동
    
    #109

--- src/components/MapBottomSheetOrganism/MapBottomSheetOrganism.tsx
+++ src/components/MapBottomSheetOrganism/MapBottomSheetOrganism.tsx
@@ -2,14 +2,13 @@ import BottomSheet, {BottomSheetFlatList} from '@gorhom/bottom-sheet';
 import {useHeaderHeight} from '@react-navigation/elements';
 import React, {useEffect, useMemo, useRef} from 'react';
 import {gestureHandlerRootHOC} from 'react-native-gesture-handler';
-import {useDispatch, useSelector} from 'react-redux';
+import {useSelector} from 'react-redux';
 
 import BoothSummaryView from '../BoothSummaryView';
 import MapNaverMapOrganism from '../MapNaverMapOrganism';
 import {BottomSheetConatiner, bottomSheetStyle, handleStyle} from './MapBottomSheetOrganism.styles';
 
 import useGetPhotoBoothLocation from 'src/querys/useGetPhotoBoothLocation';
-import {changeBottomSheetHeight} from 'src/redux/actions/MapAction';
 import {RootState} from 'src/redux/store';
 import {heightPercentage} from 'src/styles/ScreenResponse';
 import {PhotoBoothContentData} from 'src/types';
@@ -21,7 +20,6 @@ const MapBottomSheetOrganism = () => {
   const headerHeight = useHeaderHeight();
   const snapPoints = useMemo(
     () => [
-      heightPercentage(30),
       heightPercentage(130),
       valueOfPlatform({
         ios: heightPercentage(630),
@@ -30,18 +28,15 @@ const MapBottomSheetOrganism = () => {
     ],
     [],
   );
+  const focusedBooth = useSelector((state: RootState) => state.mapReducer.focusBooth);
 
   const {data} = useGetPhotoBoothLocation({longitude: 0, latitude: 0});
-  const dispatch = useDispatch();
-  const bottomSheetOnChange = (event: number) => {
-    dispatch(changeBottomSheetHeight(event));
-  };
   useEffect(() => {
-    if (data?.data.content.length) {
-      dispatch(changeBottomSheetHeight(1));
-    } else {
-      dispatch(changeBottomSheetHeight(0));
-    }
+    // if (data?.data.content.length) {
+    //   dispatch(changeBottomSheetHeight(1));
+    // } else {
+    //   dispatch(changeBottomSheetHeight(0));
+    // }
   }, [data]);
 
   return (
@@ -52,11 +47,22 @@ const MapBottomSheetOrganism = () => {
         ref={bottomSheetRef}
         backdropComponent={MapNaverMapOrganism}
         handleIndicatorStyle={handleStyle}
-        onChange={bottomSheetOnChange}
         index={sheetIndex}>
         <BottomSheetFlatList
           data={data?.data.content}
-          renderItem={({item}: {item: PhotoBoothContentData}) => <BoothSummaryView {...item} />}
+          renderItem={({item, index}: {item: PhotoBoothContentData; index: number}) =>
+            index === 0 ? (
+              focusedBooth !== null ? (
+                <BoothSummaryView {...focusedBooth} />
+              ) : (
+                <BoothSummaryView {...item} />
+              )
+            ) : item.photoBooth.id !== focusedBooth?.photoBooth.id ? (
+              <BoothSummaryView {...item} />
+            ) : (
+              <></>
+            )
+          }
         />
       </BottomSheet>
     </BottomSheetConatiner>
