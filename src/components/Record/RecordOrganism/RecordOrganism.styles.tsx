b5e7255e582e4ca6794abaa7851cabc106a208f7
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-08-15 01:58:48 +0900

    ✨ feat: add Record Page & fix getUser logic (#153)
    레코드 페이지 ui 추가 및 getuser 로직 수정
    
    #153

--- src/components/Record/RecordOrganism/RecordOrganism.styles.tsx
+++ src/components/Record/RecordOrganism/RecordOrganism.styles.tsx
@@ -1,3 +1,9 @@
 import styled from '@emotion/native';
 
 import {heightPercentage} from 'src/styles/ScreenResponse';
+
+export const NickNameContainer = styled.View({
+  width: '100%',
+  height: heightPercentage(16),
+  marginTop: heightPercentage(11),
+});
