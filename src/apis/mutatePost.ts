d212bc1bc9ff555d66344eca400fcefdf415bc71
vgihan <jum654@naver.com>
2022-08-17 14:29:24 +0900

    ✨ feat(post write): connect POST /post api in post write
    multipart/form-data로 포스트 작성 api 연동

--- src/apis/mutatePost.ts
+++ src/apis/mutatePost.ts
@@ -1,14 +1,22 @@
 import AxiosInstance from 'src/components/utils/Interceptor';
 import {PostCreateRequest} from 'src/types/post';
-import getApiServer from 'src/utils/getApiServer';
 
 const mutatePost = async (postCreateRequest: PostCreateRequest) => {
   try {
-    const {postImageList, ...restData} = postCreateRequest;
     const formData = new FormData();
-    formData.append('postImageList', postImageList);
-    formData.append('postCreateRequest', JSON.stringify(restData));
-    const result = await AxiosInstance.post(`${getApiServer}/post`, formData);
+    const {postImageList, ...restData} = postCreateRequest;
+
+    formData.append('postImageList', postImageList[0]);
+    formData.append('postCreateRequest', {
+      string: JSON.stringify(restData),
+      type: 'application/json',
+    });
+
+    const result = await AxiosInstance.post('/api/v1/post', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
 
     return result.data;
   } catch (error) {
--- src/types/post.d.ts
+++ src/types/post.d.ts
@@ -4,5 +4,7 @@ export interface PostCreateRequest {
   title: string;
   content: string;
   tagIdList: number[];
+  isPublic: boolean;
+  newTagList: string[];
   postImageList: FormImage[];
 }
