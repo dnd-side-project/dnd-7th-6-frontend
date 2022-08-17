9c9fcaed1b75dd656849ecd788ae97b9cc0a2a10
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-08-17 23:55:54 +0900

    ♻️ refactor: refactor code (#153)
    코드리뷰 받은 내용 수정
    
    #153

--- src/querys/usePatchNickName.ts
+++ src/querys/usePatchNickName.ts
@@ -4,7 +4,7 @@ import {useMutation} from 'react-query';
 import patchNickName from 'src/apis/patchNickName';
 
 const usePatchNickName = () => {
-  return useMutation(async (nickName: string) => await patchNickName(nickName), {
+  return useMutation((nickName: string) => patchNickName(nickName), {
     onError: () => Alert.alert('닉네임을 수정하는데 문제가 발생하였습니다.'),
   });
 };
