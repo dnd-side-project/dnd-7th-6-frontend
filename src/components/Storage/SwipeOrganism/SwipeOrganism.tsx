c314646e72127e0516996bf36e0c41d7af1e66ec
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-08-19 00:07:42 +0900

    ✨ feat: add ui in Storage Screen if logged out (#177)
    로그아웃시 ui 추가
    
    #177

--- src/components/Storage/SwipeOrganism/SwipeOrganism.tsx
+++ src/components/Storage/SwipeOrganism/SwipeOrganism.tsx
@@ -1,23 +1,30 @@
 import React, {useState} from 'react';
 import {View} from 'react-native';
+import {useSelector} from 'react-redux';
 
 import PhotoOrganism from '../PhotoOrganism';
 
 import LineSlideView from 'src/components/utils/LineSlideView';
+import NotLoginContainer from 'src/components/utils/NotLoginContainer';
 import useGetUserLike from 'src/querys/useGetUserLike';
+import {RootState} from 'src/redux/store';
 
 const SwipeOrganism = () => {
   const [index, setIndex] = useState(0);
   const {data} = useGetUserLike();
+  const isLoggedIn = useSelector((state: RootState) => state.userReducer.isLoggedIn);
   const items = [
     {name: '사진', count: !data ? 0 : data.imageList.length},
     {name: '부스', count: !data ? 0 : data.photoBoothList.length},
   ];
-
-  return (
+  return isLoggedIn ? (
     <LineSlideView items={items} index={index} setIndex={setIndex}>
       {!!data && <PhotoOrganism photoList={data.imageList} />}
       <View />
+    </LineSlideView>
+  ) : (
+    <LineSlideView items={items} index={index} setIndex={setIndex}>
+      <NotLoginContainer>좋아하거나 가보고 싶은 부스를 찜해보세요.</NotLoginContainer>
     </LineSlideView>
   );
 };
