80daa94c7f0a46975980d0ce0a66b8937746dac9
vgihan <jum654@naver.com>
2022-08-22 18:13:03 +0900

    ✨ feat(post write): add logic of navigate to record screen in post write

--- src/components/PostWrite/ExitCenterOrganism/ExitCenterOrganism.tsx
+++ src/components/PostWrite/ExitCenterOrganism/ExitCenterOrganism.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import FastImage from 'react-native-fast-image';
+import {useSelector} from 'react-redux';
 
 import {Container, Headline, Style} from './ExitCenterOrganism.styles';
 
+import {RootState} from 'src/redux/store';
+
 const ExitCenterOrganism = () => {
+  const {isPostModifyMode} = useSelector((state: RootState) => state.postWriteReducer);
+
   return (
     <Container>
       <FastImage source={{uri: ''}} style={Style.fastImage} />
-      <Headline>공유 완료!</Headline>
+      <Headline>{isPostModifyMode ? '수정 완료 !' : '작성 완료 !'}</Headline>
     </Container>
   );
 };
