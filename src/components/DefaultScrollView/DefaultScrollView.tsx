eea7fb87fbc301a8c346ce5efce3b86b57520266
ShinJongUng <65454966+ShinJongUng@users.noreply.github.com>
2022-09-04 00:06:30 +0900

    ✨ feat: add pull to refrsh ui (#267)
    #267

--- src/components/DefaultScrollView/DefaultScrollView.tsx
+++ src/components/DefaultScrollView/DefaultScrollView.tsx
@@ -1,13 +1,10 @@
 import React, {PropsWithChildren} from 'react';
+import {ScrollViewProps} from 'react-native';
 
 import {DefaultScroll} from './DefaultScrollView.styles';
 
-const DefaultScrollView = ({children}: PropsWithChildren) => {
-  return (
-    <DefaultScroll bounces={false} overScrollMode={'never'}>
-      {children}
-    </DefaultScroll>
-  );
+const DefaultScrollView = ({children, ...props}: PropsWithChildren<ScrollViewProps>) => {
+  return <DefaultScroll {...props}>{children}</DefaultScroll>;
 };
 
 export default DefaultScrollView;
